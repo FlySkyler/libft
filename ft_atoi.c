797f058f91c7fdbb505dbe52b12442fdbf91ead7
antoine <antoine.bonin78@gmail.com>
2024-11-17 21:55:27 +0100

    modification atoi callloc ft_itoa

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,35 +6,37 @@
 /*   By: antbonin <antbonin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 12:40:20 by antbonin          #+#    #+#             */
-/*   Updated: 2024/11/12 16:25:45 by antbonin         ###   ########.fr       */
+/*   Updated: 2024/11/17 21:48:21 by antbonin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
 
 int	ft_atoi(const char *nptr)
 {
-	int	i;
-	int	sign;
-	int	nb;
+	int		i;
+	int		sign;
+	long	nb;
 
 	nb = 0;
 	i = 0;
+	sign = 1;
 	while ((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32)
 		i++;
 	if (nptr[i] == '-' || nptr[i] == '+')
 	{
-		if (nptr[i] == '-')
+		if (nptr[i++] == '-')
 			sign = -1;
-		i++;
 	}
 	while (nptr[i] >= 48 && nptr[i] <= 57)
 	{
 		nb = nb * 10 + nptr[i] - 48;
 		i++;
+		if (nb > INT_MAX && sign != -1)
+			return (-1);
+		else if (nb < INT_MIN && sign == -1)
+			return (0);
 	}
-	if (sign == -1)
-		return (-nb);
-	else
-		return (nb);
+	return (sign * nb);
 }

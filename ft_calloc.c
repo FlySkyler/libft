aeec9d6524378815b74edf62697978e1d9d8a672
antoine <antoine.bonin78@gmail.com>
2024-11-17 16:49:15 +0100

    modification calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,19 +6,22 @@
 /*   By: antbonin <antbonin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 17:24:36 by antbonin          #+#    #+#             */
-/*   Updated: 2024/11/17 16:34:03 by antbonin         ###   ########.fr       */
+/*   Updated: 2024/11/17 16:48:22 by antbonin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
+#include <stdint.h>
 
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
-	if (count != 0 && size > __SIZE_MAX__ / count)
+	if (count == 0)
 		return (ptr = malloc(0));
+	if (size > SIZE_MAX / count)
+		return (NULL);
 	ptr = malloc(count * size);
 	if (ptr == NULL)
 		return (NULL);
